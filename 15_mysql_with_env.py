812089db438c137c2b2bd2fc383716b9e3069bb2
StevieW <pmwong008@gmail.com>
2025-07-21 17:42:39 +0800

    Wrap up SQL

--- 15_mysql_with_env.py
+++ 15_mysql_with_env.py
@@ -1,27 +1,27 @@
-# pip install mysql-connector-python
 import os
 from mysql.connector import connect, Error
 from dotenv import load_dotenv
+
+# Load Environment from .env
 load_dotenv()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         with connect(
             host=os.getenv('DB_HOST'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PWD'),
-            database=os.getenv('DB_NAME'),
+            database=os.getenv('DB_DATABASE'),
         ) as connection:
             print(connection)
-            show_table_query = """
-            DESCRIBE city
+            read_table_query = """
+               SELECT * FROM world.city;
             """
 
             with connection.cursor() as cursor:
-                cursor.execute(show_table_query)
+                cursor.execute(read_table_query)
                 result = cursor.fetchall()
                 for row in result:
                     print(row)
-
     except Error as e:
         print(e)
